f2bbf3765106f3fdd8e9b79b95d75475a3a82386
bryanbilek <bbilek35@gmail.com>
2020-07-14 23:05:58 -0500

    refactored insert after watching tomorrow's lectur

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -19,19 +19,17 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        if self.value:
-            if value < self.value:
-                if self.left is None:
-                    self.left = BSTNode(value)
-                else:
-                    self.left.insert(value)
-            elif value > self.value:
-                if self.right is None:
-                    self.right = BSTNode(value)
-                else:
-                    self.right.insert(value)
+
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
         else:
-            self.value = value
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
@@ -40,6 +38,7 @@ class BSTNode:
         pass
 
     # Return the maximum value found in the tree
+
     def get_max(self):
         pass
 
